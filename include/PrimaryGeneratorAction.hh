70d9d90d2ac39ed9b7030877c9c8135366600b17
snow john <Wolf_Smile@outlook.com>
2025-11-29 21:54:38 +0800

    Optimize PrimaryGeneratorAction and update notebook

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -1,31 +1,38 @@
 #ifndef PRIMARY_GENERATOR_ACTION_HH
 #define PRIMARY_GENERATOR_ACTION_HH
 
-#include <G4VUserPrimaryGeneratorAction.hh>
-#include <G4ParticleGun.hh>
 #include "G4GeneralParticleSource.hh"
+#include "G4GenericMessenger.hh"
+#include <G4ParticleGun.hh>
+#include <G4VUserPrimaryGeneratorAction.hh>
 #include <vector>
 
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
 public:
-    PrimaryGeneratorAction();
-    virtual ~PrimaryGeneratorAction();
+  PrimaryGeneratorAction();
+  virtual ~PrimaryGeneratorAction();
 
-    virtual void GeneratePrimaries(G4Event* event) override;
+  virtual void GeneratePrimaries(G4Event *event) override;
 
 private:
-    G4ParticleGun* fParticleGun;
+  G4ParticleGun *fParticleGun;
+  G4GenericMessenger *fMessenger;
 
-    // CsI晶体阵列参数，再次声明或传入
-    G4int fNx, fNy, fNz;
-    G4double fCrystalSize;
-    G4double fGap;
-    G4double fTotalX, fTotalY, fTotalZ;
-    G4double fStartX, fStartY, fStartZ;
+  // Configurable parameters
+  G4double fMaxEnergy;
 
-    void InitializeArrayGeometry();
+  // Cached particle definitions
+  G4ParticleDefinition *fElectron;
+  G4ParticleDefinition *fPositron;
+
+  // CsI晶体阵列参数，再次声明或传入
+  G4int fNx, fNy, fNz;
+  G4double fCrystalSize;
+  G4double fGap;
+  G4double fTotalX, fTotalY, fTotalZ;
+  G4double fStartX, fStartY, fStartZ;
+
+  void InitializeArrayGeometry();
 };
 
 #endif
-
-
--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -1,17 +1,34 @@
 // PrimaryGeneratorAction.cc
 #include "PrimaryGeneratorAction.hh"
+#include "G4GenericMessenger.hh"
 #include "G4ParticleGun.hh"
+#include "G4RandomDirection.hh"
+#include "G4SystemOfUnits.hh"
 #include "Randomize.hh"
 #include <G4Event.hh>
 #include <G4ParticleDefinition.hh>
 #include <G4ParticleTable.hh>
-#include <G4SystemOfUnits.hh>
+
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTable.hh"
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
-    : G4VUserPrimaryGeneratorAction(), fParticleGun(nullptr), fNx(8), fNy(8),
-      fNz(5), fCrystalSize(6 * cm), fGap(0.5 * cm) {
+    : G4VUserPrimaryGeneratorAction(), fParticleGun(nullptr),
+      fMaxEnergy(4 * MeV), fElectron(nullptr), fPositron(nullptr), fNx(8),
+      fNy(8), fNz(5), fCrystalSize(6 * cm), fGap(0.5 * cm) {
 
   fParticleGun = new G4ParticleGun(1);
+
+  // Cache particle definitions
+  G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+  fElectron = particleTable->FindParticle("e-");
+  fPositron = particleTable->FindParticle("e+");
+
+  // Define commands
+  fMessenger = new G4GenericMessenger(this, "/CsI/generator/",
+                                      "Primary generator control");
+  fMessenger->DeclarePropertyWithUnit("maxEnergy", "MeV", fMaxEnergy,
+                                      "Maximum energy for electrons");
 
   // 计算阵列总尺寸和起点位置
   fTotalX = fNx * fCrystalSize + (fNx - 1) * fGap;
@@ -23,14 +40,13 @@ PrimaryGeneratorAction::PrimaryGeneratorAction()
   fStartZ = -fTotalZ / 2 + fCrystalSize / 2;
 }
 
-PrimaryGeneratorAction::~PrimaryGeneratorAction() { delete fParticleGun; }
+PrimaryGeneratorAction::~PrimaryGeneratorAction() {
+  delete fParticleGun;
+  delete fMessenger;
+}
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event *event) {
   // static G4int pairCount = 0;
-
-  G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-  G4ParticleDefinition *eMinus = particleTable->FindParticle("e-");
-  G4ParticleDefinition *ePlus = particleTable->FindParticle("e+");
 
   // 随机选择一个晶体
   G4int ix = G4UniformRand() * fNx;
@@ -56,28 +72,21 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event *event) {
   G4ThreeVector vertexPos(centerX + localX, centerY + localY, centerZ + localZ);
 
   // 动能在0到8 MeV之间均匀分布
-  G4double energyMeV = G4UniformRand() * 4 * MeV;
+  G4double energyMeV = G4UniformRand() * fMaxEnergy;
 
   // 生成一对电子对，方向共线反向（假设沿随机单位矢量n和-n）
-  G4double theta = acos(1 - 2 * G4UniformRand()); // 0~pi 均匀分布
-  G4double phi = 2 * M_PI * G4UniformRand();
-
-  G4double dx = sin(theta) * cos(phi);
-  G4double dy = sin(theta) * sin(phi);
-  G4double dz = cos(theta);
-
-  G4ThreeVector dir1(dx, dy, dz);
+  G4ThreeVector dir1 = G4RandomDirection();
   G4ThreeVector dir2 = -dir1;
 
   // --- 产生电子 ---
-  fParticleGun->SetParticleDefinition(eMinus);
+  fParticleGun->SetParticleDefinition(fElectron);
   fParticleGun->SetParticleEnergy(energyMeV);
   fParticleGun->SetParticlePosition(vertexPos);
   fParticleGun->SetParticleMomentumDirection(dir1);
   fParticleGun->GeneratePrimaryVertex(event);
 
   // --- 产生正电子 ---
-  fParticleGun->SetParticleDefinition(ePlus);
+  fParticleGun->SetParticleDefinition(fPositron);
   fParticleGun->SetParticleEnergy(energyMeV);
   fParticleGun->SetParticlePosition(vertexPos);
   fParticleGun->SetParticleMomentumDirection(dir2);
